4b96463da31b572fe3101d42a33b8b682aaa8a6f
KO-KMS-AT12 <vtg0502@gmail.com>
2019-04-18 15:59:33 +0700

    thuc hanh Show System Time

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.format("Now is: " + now);
+    }
+}
